21594c56d4f65c021f87ebb858ac4556433739a5
혀느현스 <hyuns@hyuns.dev>
2022-11-11 19:27:11 +0900

    fix branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "duckie-team", // Usually your GitHub org/user name.
   projectName: "duckie-opensource", // Usually your repo name.
-  deploymentBranch: "deploy",
+  deploymentBranch: "gh-pages",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
