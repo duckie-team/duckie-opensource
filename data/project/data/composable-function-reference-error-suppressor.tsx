0a9a443785c31a7869f76abab438c6717e4f3290
Ji Sungbin <ji@sungb.in>
2022-11-15 09:00:18 +0900

    잘못된 태그 정정

--- data/project/data/composable-function-reference-error-suppressor.tsx
+++ data/project/data/composable-function-reference-error-suppressor.tsx
@@ -8,7 +8,7 @@ export const projectComposableFunctionReferenceErrorSuppressor: Project = {
   description:
     "Composable 함수를 레퍼런스 참조하게 되면 발생하는 `COMPOSABLE_FUNCTION_REFERENCE` 에러를 억제시키는 코틀린 컴파일러 플러그인 입니다. Composable 함수에 리플렉션이 필요할 때 사용할 수 있습니다.",
   iconImage: require(`@site/static/img/project/project1.png`).default,
-  tags: ["Library"],
+  tags: ["Compiler Plugin"],
   targets: [targets.kotlin, targets.jetpackCompose],
   link: "/project/composable-function-reference-error-suppressor",
   showFeaturedProjects: true,
